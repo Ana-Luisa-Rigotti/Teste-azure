4f1afb5924341f276276cc0638207714311d613f
Ana-Luisa-Rigotti <alohapovo@gmail.com>
2026-03-05 19:48:20 -0300

    teste endpoint fastapi

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Teste Azure funcionando")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"ok": True, "msg": "Deploy GitHub -> Azure funcionando 😌"}
