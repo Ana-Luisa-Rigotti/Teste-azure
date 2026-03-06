0e2815de51a15f9174b45ddbab9102b39ab73085
Ana-Luisa-Rigotti <alohapovo@gmail.com>
2026-03-05 21:01:03 -0300

    teste conexao sql

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, HTTPException
+from sqlalchemy import create_engine, text
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"ok": True, "msg": "Deploy GitHub -> Azure funcionando!"}
+engine = None
 
+@app.on_event("startup")
+def startup():
+    global engine
+    conn_str = os.getenv("SQL_CONNECTION_STRING")
+    if not conn_str:
+        raise RuntimeError("SQL_CONNECTION_STRING não configurada.")
+    engine = create_engine(conn_str, pool_pre_ping=True)
+
+@app.get("/db-test")
+def db_test():
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT 1")).scalar()
+        return {"db_ok": True, "result": result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
