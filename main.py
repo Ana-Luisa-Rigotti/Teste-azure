cb2a6d1237fdd5107d2b9d6f683fe6faaa332731
Ana-Luisa-Rigotti <alohapovo@gmail.com>
2026-03-08 21:24:57 -0300

    teste pyodbc

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 import os
-import pymssql
+import pyodbc
 from fastapi import FastAPI, HTTPException, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.templating import Jinja2Templates #integrar com o front
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+
 def get_db():
     server = os.getenv("DB_SERVER")
     database = os.getenv("DB_NAME")
+
+    if not server or not database:
+        raise RuntimeError("Variáveis DB_SERVER e DB_NAME não configuradas.")
+
+    # Se existir DB_USER e DB_PASSWORD, usa login tradicional
     user = os.getenv("DB_USER")
     password = os.getenv("DB_PASSWORD")
 
-    if not all([server, database, user, password]):
-        raise RuntimeError("Variáveis DB_* não configuradas no Azure.")
+    if user and password:
+        conn_str = (
+            "DRIVER={ODBC Driver 18 for SQL Server};"
+            f"SERVER={server};"
+            f"DATABASE={database};"
+            f"UID={user};"
+            f"PWD={password};"
+            "Encrypt=yes;"
+            "TrustServerCertificate=no;"
+        )
+    else:
+        # Se não existir usuário/senha, tenta Managed Identity
+        conn_str = (
+            "DRIVER={ODBC Driver 18 for SQL Server};"
+            f"SERVER={server};"
+            f"DATABASE={database};"
+            "Authentication=ActiveDirectoryMsi;"
+            "Encrypt=yes;"
+            "TrustServerCertificate=no;"
+        )
 
-    return pymssql.connect(
-        server=server,
-        user=user,
-        password=password,
-        database=database
-    )
+    return pyodbc.connect(conn_str)
+
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
@@ -41,6 +61,7 @@ def home(request: Request):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @app.post("/pessoas-form")
 def create_people_form(
     id: int = Form(...),
@@ -51,7 +72,7 @@ def create_people_form(
         db = get_db()
         cursor = db.cursor()
         cursor.execute(
-            "INSERT INTO Pessoas (id, nome, idade) VALUES (%s, %s, %s)",
+            "INSERT INTO Pessoas (id, nome, idade) VALUES (?, ?, ?)",
             (id, nome, idade)
         )
         db.commit()
@@ -60,6 +81,7 @@ def create_people_form(
         return RedirectResponse(url="/", status_code=303)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.get("/pessoas")
 def list_people():
